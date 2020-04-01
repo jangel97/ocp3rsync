b2ef6457f9b2e234ebb4a63fc241d1fb7fabb3a5
Your Name <you@example.com>
2020-04-01 04:47:20 -0400

    pedning to do more tests, maybe create pullsecret

---
+++ README.md
@@ -0,0 +1,71 @@
+# openshift-rsyncer
+
+# BINARY BUILD:
+
+oc new-project prueba
+
+oc create -f conf/rbac.yml
+
+oc new-build --name rsync --binary --strategy docker #create build from local dir
+
+oc start-build rsync --from-dir . --follow
+
+oc new-app rsync
+
+oc patch dc/rsync --patch '{"spec":{"template":{"spec":{"serviceAccountName": "backup-sacc"}}}}'
+ 
+oc create configmap info --from-file=info.json 
+
+oc set volumes dc rsync --add --name=info --type=configmap --configmap-name=info --mount-path=/opt/app-root/info.json --sub-path=info.json
+
+-------------------------------------------------
+
+
+https://github.com/kubernetes-client/python/blob/02ef5be4ecead787961037b236ae498944040b43/examples/pod_exec.py
+
+https://github.com/openshift/openshift-restclient-python
+
+https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-terminal-in-python
+
+
+https://stackoverflow.com/questions/42363105/permission-denied-mkdir-in-container-on-openshift
+
+
+https://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on
+
+https://stackoverflow.com/questions/44035287/check-key-exist-in-python-dict/44035382
+
+
+https://kubernetes.io/docs/tasks/configure-pod-container/configure-persistent-volume-storage/  #definicion yaml de pod con pvc
+
+CREAR SECRETO PULL IMAGE:
+https://kubernetes.io/docs/concepts/containers/images/#specifying-imagepullsecrets-on-a-pod
+
+https://docs.openshift.com/container-platform/3.11/dev_guide/secrets.html
+
+https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/
+oc adm pod-network join-projects --to=prueba primary default #prueba, proyecto donde desplegue el pod rsyncer, primary y default proyectos con volumenes
+#incluir en el condigo
+
+oc adm pod-network isolate-projects prueba primary default
+#isolate project, default is not possible to isolate
+
+
+#rsync -a -v --rsh='oc rsh -n default' docker-registry-1-fp2f7:/registry  primary/primary-primary-pgdata/   -> v2.go:147] write /dev/stdout: resource temporarily unavailable
+
+#oc adm pod-network make-projects-global <project1> <project2> -> opcion interesante
+
+ oc set volumes dc nagiosdocker --add --name=htpasswd --type=configmap --configmap-name=htpasswd.users --mount-path=/usr/local/nagios/etc/htpasswd.users --sub-path=htpasswd.users
+
+
+ oc set volumes dc/drupal-openshift --add --name=mysql --type=pvc --mount-path=/var/lib/mysql/data --claim-size=10Gi
+
+
+ oc set volumes dc/docker-registry --add --name=registrynfs --type=pvc --mount-path=/registry --claim-name=registrynfs-claim 
+
+
+#git log --graph --decorate --oneline
+
+https://github.com/learnk8s/templating-kubernetes/tree/master/python
+
+https://github.com/kubernetes-client/python/tree/master/examples
---
+++ oc-rsync.py
@@ -0,0 +1,103 @@
+import os,sys,json,time, yaml 
+from kubernetes import client, config
+from openshift.dynamic import DynamicClient
+from pathlib import Path
+from pprint import pprint
+from jinja2 import Template 
+
+v1_pvc=None
+v1_pod=None
+
+ROOT_BACKUP_FOLDER="/backup"
+
+RSYNC_OPTIONS=" --delete=true --progress=true"
+
+def print_map(map):
+   print(json.dumps(map,indent=2))	#embellecer salida diccionario, proposito debug
+
+def initialize():
+   if "OPENSHIFT_BUILD_NAME" in os.environ:	#si el script se esta ejecutando en un pod... cargar el contexto desde dento del pod
+      config.load_incluster_config()
+      file_namespace = open(
+          "/run/secrets/kubernetes.io/serviceaccount/namespace", "r"
+      )
+      if file_namespace.mode == "r":
+         namespace = file_namespace.read()
+         print("namespace: %s\n" %(namespace))
+         global ROOT_BACKUP_FOLDER
+         ROOT_BACKUP_FOLDER="/opt/app-root/backup"
+   else:
+      config.load_kube_config()			#sino... cargar el contexto desde ~/.kube/config
+   k8s_config = client.Configuration()		
+   k8s_client = client.api_client.ApiClient(configuration=k8s_config)
+   dyn_client = DynamicClient(k8s_client)	#crear objeto cliente API openshift desde configuracion kubernetes
+   global v1_pvc
+   global v1_pod
+   v1_pvc = dyn_client.resources.get(api_version='v1',kind='PersistentVolumeClaim')
+   v1_pod = dyn_client.resources.get(api_version='v1',kind='Pod')
+
+def treat_pvcs():
+   info=json.loads(open('info.json').read())
+   for params in info['SOURCE_VOLUMES']:
+      namespace=params['NAMESPACE']
+      pods=v1_pod.get(namespace=namespace).to_dict()['items']
+      for pvc in params['PVCS']:
+         pvc=v1_pvc.get(namespace=namespace,name=pvc).to_dict()  #CHEQUEAR, QUE PASA SI EL PVC NO SE ENCUENTRA EN EL PROYECTO, O DIRECTAMENTE NI EXISTE
+         if pvc['status']['phase'] == "Bound":
+            rsync(pods,pvc,namespace)
+         else: 
+            print("ERROR: PVC: "+ str(pvc['metadata']['name']) + ", proyecto: " + str(namespace) + " has status pending..." )
+
+def rsync(pods,pvc,namespace):
+   print("\n\n----------------------------------\nNAMESPACE: " + namespace)
+   print("PVC: " + pvc['metadata']['name'])
+   for pod in pods: 
+      volume_pod_pvc=list(filter(lambda volume: volume.get('persistentVolumeClaim',{}).get('claimName','')==pvc['metadata']['name'],pod['spec']['volumes'])) #se comprueba que el pod tenga el pvc montado
+      if volume_pod_pvc and pod['status']['phase']=='Running': #si tiene volumen asociado al pvc y el status es 'Running', se recorreran los containers para coger el que atache el volumen
+         for container in pod['spec']['containers']:	
+            volume_mount=list(filter(lambda volume_mount: volume_mount['name']==volume_pod_pvc[0]['name'],container['volumeMounts']))
+            if volume_mount: break #se encontro el volume_mount 
+         pod_path=volume_mount[0]['mountPath']  #siempre deberia encontrarse el volume_mount, muy extrano tiene que ser para que no
+         pod_name=pod['metadata']['name']
+         Path(ROOT_BACKUP_FOLDER + "/"+namespace+"/"+pvc['metadata']['name']).mkdir(parents=True, exist_ok=True)
+         command="oc -n " + namespace + " rsync " + pod_name  +":" + pod_path + " "+ ROOT_BACKUP_FOLDER + "/" +namespace + "/"+pvc['metadata']['name'] + RSYNC_OPTIONS
+         print(command)
+         print(os.popen(command).read()) 
+         return   #la funcion se acaba porque ya se encontro un pod donde el pvc estaba montado, si el codigo continua su ejecucion es porque el pvc esta 'Bound' pero ningun pod lo tiene montado
+   print('TEMPORARY POOOOOOOOOOOOOD') 
+   pod_yaml=open('pod.yaml','r').read()
+   template=Template(pod_yaml)
+   params_pod_temporary={
+                  'backup_path': '/backup',
+                  'name': 'rsyncer-pod-agent',
+                  'image':'docker-registry.default.svc:5000/default/rsyncer:latest',
+                  #'image': 'registry.redhat.io/openshift3/ose-cli:v3.11',
+                  'pvc': pvc['metadata']['name'],
+                  'volume_name': str(pvc['metadata']['namespace']) + '-pvc-volume' }
+
+   pod_definition=template.render(params=params_pod_temporary)
+   pod_template=yaml.load(pod_definition, Loader=yaml.FullLoader)
+   resp = v1_pod.create(body=pod_template,namespace=namespace)   #controlar excepcion, si no se crea bien
+   tt=0
+   for event in v1_pod.watch(namespace=namespace):
+      status=event['raw_object']['status']['phase']
+      print("\nPod agent status is... " + status+", namespace: " + namespace)
+      if status=="Running": break
+      time.sleep(3)
+      tt=tt+3
+      if tt>=(300/3): 
+         print('ERROR: Pod temporal, no consiguio levantar en proyecto: ' + namespace)
+         return
+ 
+   print("Attempting rsync against temporary pod in project: "+namespace) 
+   command="oc -n " + namespace + " rsync "+ params_pod_temporary['name']  +":" + params_pod_temporary['backup_path'] + " "+ ROOT_BACKUP_FOLDER +"/" +namespace + "/"+pvc['metadata']['name']
+  
+   Path(ROOT_BACKUP_FOLDER + "/"+namespace+"/"+pvc['metadata']['name']).mkdir(parents=True, exist_ok=True)
+   print(command)
+   print(os.popen(command).read()) 
+   v1_pod.delete(name=params_pod_temporary['name'] , namespace=namespace)
+   print("Temporary pod in namespace " + namespace + " was deleted")
+ 
+if __name__ == "__main__":
+   initialize()
+   treat_pvcs()
